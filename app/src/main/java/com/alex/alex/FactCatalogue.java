1d39c827c62f76fc230b993869f360b53bd93511
Paget96 <mpaget96@gmail.com>
2017-04-11 22:49:10 +0200

    Removed \n

--- app/src/main/java/com/alex/alex/FactCatalogue.java
+++ app/src/main/java/com/alex/alex/FactCatalogue.java
@@ -10,8 +10,7 @@ public class FactCatalogue {
         facts.add(new Fact("In the U.S., if you are delayed by an airline for more than 4 hours for actions within their control (overbooking)"
                 ,"you are entitled to 4 times the face vale of the ticket, capped at $1300"));
 
-        facts.add(new Fact("\n" +
-                "When United Airlines broke a man's $4500 guitar and refused to pay for it in 2009, he wrote a protest song."
+        facts.add(new Fact("When United Airlines broke a man's $4500 guitar and refused to pay for it in 2009, he wrote a protest song."
         ,"Over the next 4 weeks, United's stock price fell 10%, costing stockholders $180 million."));
     }
 
